e279750ec389e6aadee058ccb1d751261632bcb4
Isira <88isira88@gmail.com>
2021-07-07 18:36:29 -0400

    added point blocks

--- Assets/Game/Scripts/LevelManager.cs
+++ Assets/Game/Scripts/LevelManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class LevelManager : MonoBehaviour
 {
     public GameObject player_prefab;
     public GameObject block_prefab;
+    public GameObject finish_prefab;
+    public GameObject point_prefab;
+    public GameObject bonus_prefab;
 
     public Transform start_pos;
     public GameObject start_block;
@@ -35,15 +38,34 @@ public class LevelManager : MonoBehaviour
         generated_folder = new GameObject("generated_folder");
         generated_folder.transform.parent = transform;
 
-
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i <= 2; i++)
         {
-            GameObject gameObject = Instantiate(block_prefab);
-            gameObject.transform.parent = generated_folder.transform;
-            gameObject.transform.position = Vector3.forward * bounds.max.z;
-            gameObject.transform.position += Vector3.forward * gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().bounds.size.z/2;
-            bounds = gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().bounds;
+            PlaceObject(Instantiate(block_prefab));
         }
+
+        PlaceObject(Instantiate(finish_prefab));
+
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject _ = Instantiate(point_prefab);
+            _.transform.Find("ground").GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f));
+            _.transform.Find("text").GetComponent<TextMeshPro>().SetText(""+(i+1));
+            PlaceObject(_);
+        }
+
+        PlaceObject(Instantiate(bonus_prefab));
+
+
+
+
+    }
+
+    public void PlaceObject(GameObject obj)
+    {
+        obj.transform.parent = generated_folder.transform;
+        obj.transform.position = Vector3.forward * bounds.max.z;
+        obj.transform.position += Vector3.forward * obj.transform.Find("ground").GetComponent<MeshRenderer>().bounds.size.z / 2;
+        bounds = obj.transform.Find("ground").GetComponent<MeshRenderer>().bounds;
     }
 
     public void OnMenu()
