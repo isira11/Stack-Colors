534ed63dbf2f90ab85685f4ced1212c05dc101f9
Isira <88isira88@gmail.com>
2021-07-14 15:09:43 -0400

    fixed powerups ui

--- Assets/Game/Scripts/Upgrade.cs
+++ Assets/Game/Scripts/Upgrade.cs
@@ -3,33 +3,50 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Upgrade 
+public class Upgrade : MonoBehaviour
 {
-    public string upgrade_name;
+    public Type upgrade_type;
     public int price_divider;
+    public int price_multipler;
 
-    public TextMeshPro price_txt;
-    public TextMeshPro level_txt;
+    public TextMeshProUGUI price_txt;
+    public TextMeshProUGUI level_txt;
+    public TextMeshProUGUI resource_txt;
+    public TextMeshProUGUI upgrade_txt;
+
+
+    string upgrade_name;
+
+    public enum Type
+    {
+        KICK,
+        BONUS,
+        STACK
+    }
 
     private void Start()
     {
+        upgrade_name = upgrade_type.ToString();
         UpdateUI(GetLevel(), GetPrice());
     }
 
     public void LevelUp()
     {
         int current_level = GetLevel();
+        current_level++;
+        PlayerPrefs.SetInt(upgrade_name, current_level);
+        UpdateUI(GetLevel(), GetPrice());
     }
 
     private int GetLevel()
     {
-        return PlayerPrefs.GetInt(upgrade_name, 0);
+        return PlayerPrefs.GetInt(upgrade_name, 1);
     }
 
     private int GetPrice()
     {
         int current_level = GetLevel();
-        return (current_level * current_level) / price_divider;
+        return ((current_level * current_level) / price_divider)* price_multipler;
     }
 
     public bool Transaction(int price)
@@ -47,7 +64,31 @@ public class Upgrade
 
     public virtual void UpdateUI(int level, int price)
     {
-        price_txt.SetText(price + "");
-        level_txt.SetText(level + "");
+        switch (upgrade_type)
+        {
+            case Type.KICK:
+
+                resource_txt.SetText("x" + level * 5);
+                upgrade_txt.SetText("+" + 5);
+                level_txt.SetText("" + level);
+                price_txt.SetText(price + "$");
+
+                break;
+            case Type.BONUS:
+                resource_txt.SetText("x" + (1 + level * 0.2f));
+                upgrade_txt.SetText("+" + 0.2);
+                level_txt.SetText("" + level);
+                price_txt.SetText(price + "$");
+
+                break;
+            case Type.STACK:
+                resource_txt.SetText(""+level);
+                upgrade_txt.SetText("+" + 1);
+                level_txt.SetText("" + level);
+                price_txt.SetText(price + "$");
+                break;
+            default:
+                break;
+        }
     }
 }
